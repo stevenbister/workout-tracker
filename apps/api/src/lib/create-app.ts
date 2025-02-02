41181ef645dc16665605e7c9336abe3592d88cba
Steven Bister <stevebister@gmail.com>
2025-02-02 13:16:19 +0000

    mount auth adapter and set up cors

--- apps/api/src/lib/create-app.ts
+++ apps/api/src/lib/create-app.ts
@@ -1,14 +1,15 @@
 import { Hook, OpenAPIHono } from '@hono/zod-openapi';
+import { cors } from 'hono/cors';
 import { logger } from 'hono/logger';
 
-import { authAdapter } from '@/middlewares/better-auth';
+import { authAdapter } from '@/middlewares/auth-adapter';
 import { dbConnect } from '@/middlewares/db-connect';
 import { notFound } from '@/middlewares/not-found';
 import { onError } from '@/middlewares/on-error';
 import type { AppBindings } from '@/types';
 
-import { cors } from 'hono/cors';
 import { STATUS } from './constants/http-status-codes';
+import { PREFIX } from './constants/misc';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const defaultHook: Hook<any, any, any, any> = async (result, c) => {
@@ -38,16 +39,22 @@ export default function createApp() {
     app.use(dbConnect);
     app.use(authAdapter);
 
+    app.on(['POST', 'GET'], `${PREFIX}/auth/**`, (c) => {
+        const auth = c.get('authAdapter');
+
+        return auth.handler(c.req.raw);
+    });
+
     app.use(
         cors({
-        origin: (_, c) =>  c.env.BETTER_AUTH_URL,
-		allowHeaders: ["Content-Type", "Authorization"],
-		allowMethods: ["POST", "GET", "OPTIONS"],
-		exposeHeaders: ["Content-Length"],
-		maxAge: 600,
-		credentials: true,
-    })
-    )
+            origin: (_, c) => c.env.BASE_API_URL,
+            allowHeaders: ['Content-Type', 'Authorization'],
+            allowMethods: ['POST', 'GET', 'OPTIONS'],
+            exposeHeaders: ['Content-Length'],
+            maxAge: 600,
+            credentials: true,
+        })
+    );
 
     app.use(logger());
 
