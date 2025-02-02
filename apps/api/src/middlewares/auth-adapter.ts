ff17d4fba62025bb03ff8fdc7a69833c9c0eaa57
Steven Bister <stevebister@gmail.com>
2025-02-02 13:50:18 +0000

    move better-auth out of core and into api

--- apps/api/src/middlewares/auth-adapter.ts
+++ apps/api/src/middlewares/auth-adapter.ts
@@ -1,10 +1,32 @@
+import bcrypt from 'bcryptjs';
+import { betterAuth } from 'better-auth';
+import { drizzleAdapter } from 'better-auth/adapters/drizzle';
 import { createMiddleware } from 'hono/factory';
-
-import { authAdapter as auth } from '@repo/core/auth/server';
 
 import { AppBindings } from '@/types';
 
 export const authAdapter = createMiddleware<AppBindings>(async (c, next) => {
-    c.set('authAdapter', auth(c.get('db'), c.env.BASE_API_URL));
+    c.set(
+        'authAdapter',
+        betterAuth({
+            database: drizzleAdapter(c.get('db'), {
+                provider: 'sqlite',
+            }),
+            baseURL: c.env.BASE_API_URL,
+            emailAndPassword: {
+                enabled: true,
+                password: {
+                    hash: async (password: string) => {
+                        const salt = await bcrypt.genSalt(10);
+
+                        return bcrypt.hash(password, salt);
+                    },
+                    verify: async ({ hash, password }) =>
+                        await bcrypt.compare(password, hash),
+                },
+            },
+        })
+    );
+
     await next();
 });
--- packages/core/src/auth/client.ts
+++
@@ -1,6 +0,0 @@
-import { createAuthClient } from 'better-auth/react';
-
-export const authClient = (baseURL: string) =>
-    createAuthClient({
-        baseURL,
-    });
