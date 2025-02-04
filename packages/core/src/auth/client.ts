61f1c8c964bf92d38c679a11d652291e13ca6f70
Steven Bister <stevebister@gmail.com>
2025-02-04 11:45:09 +0000

    fix type error

--- packages/core/src/auth/client.ts
+++ packages/core/src/auth/client.ts
@@ -2,7 +2,10 @@ import { createAuthClient } from 'better-auth/react';
 
 import { API_PREFIX } from '../constants/misc.js';
 
-export const authClient = createAuthClient({
-    // TODO: make this dynamic or update with env var
-    baseURL: `http://localhost:8787${API_PREFIX}/auth`, // the base url of your auth server
-});
+// Setting return type like this to fix type-error
+export const authClient: ReturnType<typeof createAuthClient> = createAuthClient(
+    {
+        // TODO: make this dynamic or update with env var
+        baseURL: `http://localhost:8787${API_PREFIX}/auth`, // the base url of your auth server
+    }
+);
