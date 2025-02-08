40813ac4915ae10e20bdbceec5c09dd37f70e1e6
Steven Bister <stevebister@gmail.com>
2025-02-08 16:38:19 +0000

    setup single exercise route

--- apps/api/src/routes/exercises/exercises.routes.ts
+++ apps/api/src/routes/exercises/exercises.routes.ts
@@ -11,40 +11,43 @@ import { jsonContent } from '@/lib/utils/jsonContent';
 
 const tags = ['exercises'];
 
+const exerciseWithMuscleGroupsSchema = z.array(
+    exerciseSchema
+        .pick({
+            id: true,
+            name: true,
+        })
+        .merge(
+            z.object({
+                primaryMuscleGroups: z.array(z.string()),
+                secondaryMuscleGroups: z.array(z.string()),
+            })
+        )
+);
+
 export const list = createRoute({
     path: GET_ALL_EXERCISES,
     method: 'get',
     tags,
     responses: {
         [STATUS.OK.CODE]: jsonContent(
-            z.array(
-                exerciseSchema
-                    .pick({
-                        id: true,
-                        name: true,
-                    })
-                    .merge(
-                        z.object({
-                            primaryMuscleGroups: z.array(z.string()),
-                            secondaryMuscleGroups: z.array(z.string()),
-                        })
-                    )
-            ),
+            exerciseWithMuscleGroupsSchema,
             'List of all exercises'
         ),
     },
 });
 
-export const getOne = createRoute({
+export const getById = createRoute({
     path: GET_EXERCISE_BY_ID,
     method: 'get',
     tags,
     responses: {
-        [STATUS.OK.CODE]: jsonContent(exerciseSchema, 'Singular exercise'),
-        // [STATUS.NOT_FOUND.CODE]:
-        // [STATUS.UNPROCESSABLE_ENTITY.CODE] id params error
+        [STATUS.OK.CODE]: jsonContent(
+            exerciseWithMuscleGroupsSchema,
+            'Singular exercise'
+        ),
     },
 });
 
 export type ListRoute = typeof list;
-export type GetOneRoute = typeof getOne;
+export type GetByIdRoute = typeof getById;
