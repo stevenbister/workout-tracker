12f41b2438c4469fcb6ac7f6d5d703effde531b7
Steven Bister <stevebister@gmail.com>
2025-02-08 16:37:59 +0000

    organise

--- apps/api/src/lib/create-app.ts
+++ apps/api/src/lib/create-app.ts
@@ -1,20 +1,21 @@
 import type { Hook } from '@hono/zod-openapi';
 import { OpenAPIHono } from '@hono/zod-openapi';
+import { except } from 'hono/combine';
 import { cors } from 'hono/cors';
 import { csrf } from 'hono/csrf';
 import { logger } from 'hono/logger';
+import { secureHeaders } from 'hono/secure-headers';
+
+import { AUTH } from '@repo/core/constants/paths';
 
 import { authAdapter } from '@/middlewares/auth-adapter';
+import { checkIsAuthenticated } from '@/middlewares/check-is-authenticated';
 import { dbConnect } from '@/middlewares/db-connect';
 import { notFound } from '@/middlewares/not-found';
 import { onError } from '@/middlewares/on-error';
 import { session } from '@/middlewares/session';
 import type { AppBindings } from '@/types';
 
-import { checkIsAuthenticated } from '@/middlewares/check-is-authenticated';
-import { AUTH } from '@repo/core/constants/paths';
-import { except } from 'hono/combine';
-import { secureHeaders } from 'hono/secure-headers';
 import { STATUS } from './constants/http-status-codes';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -64,16 +65,15 @@ export default function createApp() {
         })
     );
 
-    app.use(secureHeaders())
+    app.use(secureHeaders());
 
     app.use(dbConnect);
     app.use(authAdapter);
 
     app.use(session);
+    app.use('*', except(`${AUTH}/*`, checkIsAuthenticated));
 
     app.use(logger());
-
-    app.use("*", except(`${AUTH}/*`, checkIsAuthenticated));
 
     app.notFound(notFound);
     app.onError(onError);
