5959b3a9e78b610844c13d6edf711149fa4813cb
Steven Bister <stevebister@gmail.com>
2025-02-10 15:45:05 +0000

    add routines endoint paths

--- packages/core/src/constants/paths.ts
+++ packages/core/src/constants/paths.ts
@@ -7,4 +7,7 @@ export const GET_ALL_MUSCLE_GROUPS = `${API_PREFIX}/muscle-groups` as const;
 
 export const GET_ALL_EQUIPMENT = `${API_PREFIX}/equipment` as const;
 
+export const GET_ALL_ROUTINES = `${API_PREFIX}/routines` as const;
+export const GET_ROUTINE_BY_ID = `${API_PREFIX}/routines/:id` as const;
+
 export const AUTH = `${API_PREFIX}/auth` as const;
