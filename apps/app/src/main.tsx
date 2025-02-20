3bc8d30d8e5515a722636b594d1e9e512c2383f3
Steven Bister <stevebister@gmail.com>
2025-02-20 16:15:33 +0000

    add global styles to app

--- apps/app/src/main.tsx
+++ apps/app/src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
+import '@repo/ui/styles/main.scss';
+
 import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
