074b55619f448893a7c65668106687b642107734
Steven Bister <stevebister@gmail.com>
2025-02-21 15:01:33 +0000

    install router

--- apps/app/vite.config.ts
+++ apps/app/vite.config.ts
@@ -1,3 +1,12 @@
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
+import { defineConfig } from 'vite';
+
 import config from '@repo/vite-config/vite';
 
-export default config;
+export default defineConfig({
+    ...config,
+    plugins: [
+        TanStackRouterVite({ autoCodeSplitting: true }),
+        ...config.plugins,
+    ],
+});
