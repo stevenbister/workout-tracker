e88f18f9c46f9ae02264d0fb2101f87de92aeee8
Steven Bister <stevebister@gmail.com>
2025-02-21 15:04:49 +0000

    add eslint router plugin

--- apps/app/eslint.config.js
+++ apps/app/eslint.config.js
@@ -1,7 +1,9 @@
 import { config } from '@repo/eslint-config/react';
+import pluginRouter from '@tanstack/eslint-plugin-router';
 
 /** @type {import("eslint").Linter.Config} */
 export default [
+    ...pluginRouter.configs['flat/recommended'],
     ...config,
     {
         ignores: ['./src/routeTree.gen.ts'],
