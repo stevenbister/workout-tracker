1c1927e683a2f75bded75a62c2db5b57bfaf846e
Steven Bister <stevebister@gmail.com>
2025-02-21 15:04:55 +0000

    clean up comments

--- apps/app/src/routes/__root.tsx
+++ apps/app/src/routes/__root.tsx
@@ -3,9 +3,8 @@ import { lazy } from 'react';
 
 const TanStackRouterDevtools =
     process.env.NODE_ENV === 'production'
-        ? () => null // Render nothing in production
+        ? () => null
         : lazy(() =>
-              // Lazy load in development
               import('@tanstack/router-devtools').then((res) => ({
                   default: res.TanStackRouterDevtools,
               }))
