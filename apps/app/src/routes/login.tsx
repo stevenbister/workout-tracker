b92adeb85b27154cad9d72a3a33e548c2ada1730
Steven Bister <stevebister@gmail.com>
2025-02-25 15:44:40 +0000

    refirect logged in user from login route

--- apps/app/src/routes/login.tsx
+++ apps/app/src/routes/login.tsx
@@ -1,8 +1,20 @@
-import { createFileRoute } from '@tanstack/react-router';
+import { createFileRoute, redirect } from '@tanstack/react-router';
+
+import { authClient } from '@repo/core/auth/client';
 
 import { LoginForm } from '@/components/login-form/login-form';
+import { ROUTES } from '@/constants';
 
 export const Route = createFileRoute('/login')({
+    beforeLoad: async () => {
+        const session = await authClient.getSession();
+
+        if (session.data) {
+            throw redirect({
+                to: ROUTES.ROOT,
+            });
+        }
+    },
     component: RouteComponent,
 });
 
--- apps/app/src/tests/routes/login.test.tsx
+++ apps/app/src/tests/routes/login.test.tsx
@@ -6,6 +6,8 @@ import {
 } from '@tanstack/react-router';
 import { render, screen, waitFor } from '@testing-library/react';
 
+import { type AuthClient, authClient } from '@repo/core/auth/client';
+
 import { ROUTES } from '@/constants';
 import { Route as loginRoute } from '@/routes/login';
 
@@ -13,6 +15,36 @@ vi.mock('@tanstack/react-router', async (importOriginal) => ({
     ...(await importOriginal()),
     useSearch: vi.fn(),
 }));
+
+vi.mock('@repo/core/auth/client', () => ({
+    authClient: {
+        getSession: vi.fn(),
+        signIn: {
+            email: vi.fn(),
+        },
+    },
+}));
+
+type SetupOptions = {
+    getSession: ReturnType<AuthClient['getSession']>;
+};
+
+const defaultSession = {
+    data: null,
+    error: null,
+};
+
+const setup = async (options?: SetupOptions) => {
+    vi.mocked(authClient.getSession).mockReturnValueOnce(
+        options?.getSession ?? defaultSession
+    );
+
+    const router = createRouter({
+        routeTree: loginRoute,
+    }) as never;
+
+    await waitFor(() => render(<RouterProvider router={router} />));
+};
 
 let history: RouterHistory;
 
@@ -25,15 +57,24 @@ afterEach(() => {
 });
 
 it('renders the login route', async () => {
-    const router = createRouter({ routeTree: loginRoute, history }) as never;
-
-    await waitFor(() => render(<RouterProvider router={router} />));
-
-    screen.debug();
+    await setup();
 
     expect(
-        screen.getByRole('heading', {
+        await screen.findByRole('heading', {
             name: 'Login',
         })
     ).toBeInTheDocument();
+});
+
+it('redirects the user to the root page if they are authenticated', async () => {
+    await setup({
+        getSession: {
+            data: {
+                user: {},
+            },
+            error: null,
+        },
+    });
+
+    expect(window.location.pathname).toBe(ROUTES.ROOT);
 });
