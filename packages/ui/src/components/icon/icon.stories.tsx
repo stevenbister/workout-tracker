5044fa06e6b328e6e2709df84fd198166ec158df
Steven Bister <stevebister@gmail.com>
2025-03-06 16:42:44 +0000

    add icon component

---
+++ packages/ui/src/components/icon/icon.stories.tsx
@@ -0,0 +1,46 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Icon, SpritesheetProvider } from './icon';
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta: Meta<typeof Icon> = {
+    title: 'Icon',
+    component: Icon,
+    decorators: [
+        (Story) => (
+            <SpritesheetProvider spriteSheetPath="/spritesheet.svg">
+                <Story />
+            </SpritesheetProvider>
+        ),
+    ],
+    render: (args) => <Icon {...args} />,
+    parameters: {
+        // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    },
+    // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+    tags: ['autodocs'],
+    // More on argTypes: https://storybook.js.org/docs/api/argtypes
+    argTypes: {},
+    // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
+    args: {},
+};
+export default meta;
+
+type Story = StoryObj<typeof Icon>;
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const Default: Story = {
+    args: {
+        spriteId: 'success',
+    },
+};
+
+export const CustomColor: Story = {
+    args: {
+        spriteId: 'close',
+        style: {
+            color: 'transparent',
+            stroke: 'var(--c-grey-500)',
+        },
+    },
+};
