fbdc63401a80dc0c3eba4a414bfd95a075a49acb
Steven Bister <stevebister@gmail.com>
2025-03-15 11:12:24 +0000

    update auth client test

--- packages/core/src/auth/client.test.ts
+++ packages/core/src/auth/client.test.ts
@@ -5,14 +5,19 @@ import { authClient } from './client';
 
 vi.mock('better-auth/react');
 
+const apiUrl = import.meta.env.VITE_API_URL;
+const apiKey = import.meta.env.VITE_API_KEY;
+
 it('creates auth client with correct baseURL', async () => {
     // eslint-disable-next-line @typescript-eslint/no-unused-expressions
     authClient;
 
-    expect(createAuthClient).toHaveBeenNthCalledWith(
-        1,
-        expect.objectContaining({
-            baseURL: `http://localhost:8787${API_PREFIX}/auth`,
-        })
-    );
+    expect(createAuthClient).toHaveBeenNthCalledWith(1, {
+        baseURL: `${apiUrl}${API_PREFIX}/auth`,
+        fetchOptions: {
+            headers: {
+                'x-api-key': apiKey,
+            },
+        },
+    });
 });
