799ea6ddaf8e8812c9e9fa9f2ddbdfa9caf84487
Steven Bister <stevebister@gmail.com>
2025-03-25 14:14:20 +0000

    add routine group path

--- packages/core/src/constants/paths.ts
+++ packages/core/src/constants/paths.ts
@@ -8,5 +8,6 @@ export const EQUIPMENT = `${API_PREFIX}/equipment` as const;
 
 export const ROUTINES = `${API_PREFIX}/routines` as const;
 export const CREATE_ROUTINE = `${ROUTINES}/create` as const;
+export const ROUTINE_GROUPS = `${ROUTINES}/groups` as const;
 
 export const AUTH = `${API_PREFIX}/auth` as const;
