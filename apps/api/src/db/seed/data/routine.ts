607f9b3ebafe427b2bc628e5422c9dbca6dd1845
Steven Bister <stevebister@gmail.com>
2025-03-25 14:14:09 +0000

    add routine group seed data

--- apps/api/src/db/seed/data/routine.ts
+++ apps/api/src/db/seed/data/routine.ts
@@ -1,29 +1,35 @@
 import { mockUser } from '@/__mocks__/session';
 import type { InsertRoutine } from '@/db/schema/routine';
 
+import { routineGroupData } from './routine-groups';
+
 export const routineData: InsertRoutine[] = [
     {
         id: 1,
         name: 'Push',
         userId: mockUser!.id,
         description: 'lorem ipsum',
+        routineGroupId: routineGroupData[0]!.id,
     },
     {
         id: 2,
         name: 'Pull',
         userId: mockUser!.id,
         description: 'lorem ipsum',
+        routineGroupId: routineGroupData[0]!.id,
     },
     {
         id: 3,
         name: 'Legs',
         userId: mockUser!.id,
         description: 'lorem ipsum',
+        routineGroupId: routineGroupData[0]!.id,
     },
     {
         id: 4,
         name: 'Full body',
         userId: '8910',
         description: 'lorem ipsum',
+        routineGroupId: routineGroupData[1]!.id,
     },
 ];
