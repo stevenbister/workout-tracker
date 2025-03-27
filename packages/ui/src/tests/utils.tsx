fa0c1e23cd8d8a6794cb8791fc8b8781ea3a8c21
Steven Bister <stevebister@gmail.com>
2025-03-27 19:33:32 +0000

    add render with router util

--- packages/ui/src/tests/utils.tsx
+++ packages/ui/src/tests/utils.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable no-restricted-imports, react-refresh/only-export-components */
+import { RouterProvider } from '@tanstack/react-router';
 import {
     type RenderOptions,
     type RenderResult,
@@ -8,18 +9,46 @@ import type { ReactElement, ReactNode } from 'react';
 
 import { SpritesheetProvider } from '../components/icon/icon';
 import { Toaster } from '../components/toast/toast';
+import { router as defaultRouter } from '../mocks/mock-router';
 
-const Wrapper = ({ children }: { children: ReactNode }) => (
+type WrapperProps = {
+    children: ReactNode;
+    router?: typeof defaultRouter;
+};
+
+const Wrapper = ({ children }: WrapperProps) => (
     <SpritesheetProvider>
         {children}
         <Toaster />
     </SpritesheetProvider>
 );
 
+const WrapperWithRoutes = ({
+    children,
+    router = defaultRouter,
+}: WrapperProps) => (
+    <Wrapper>
+        <RouterProvider router={router} defaultComponent={() => children} />
+    </Wrapper>
+);
+
 const customRender = (
     ui: ReactElement,
-    options?: Omit<RenderOptions, 'queries'>
+    options?: Omit<RenderOptions, 'wrapper'>
 ): RenderResult => render(ui, { wrapper: Wrapper, ...options });
 
+const customRenderWithRouter = (
+    ui: ReactElement,
+    router?: typeof defaultRouter,
+    options?: Omit<RenderOptions, 'wrapper'>
+): RenderResult => {
+    return render(ui, {
+        wrapper: ({ children }) => (
+            <WrapperWithRoutes router={router}>{children}</WrapperWithRoutes>
+        ),
+        ...options,
+    });
+};
+
 export * from '@testing-library/react';
-export { customRender as render };
+export { customRender as render, customRenderWithRouter as renderWithRouter };
