444f158620c502fb1dc6a5c314a338f70637c232
Steven Bister <stevebister@gmail.com>
2025-04-24 14:39:47 +0100

    move content into routines file

--- apps/app/src/components/start-routine-button/start-routine-button.tsx
+++ apps/app/src/components/start-routine-button/start-routine-button.tsx
@@ -1,5 +1,5 @@
 import { Button } from '@repo/ui/components/button';
 
-import content from '@/content/workouts.json';
+import content from '@/content/routines.json';
 
 export const StartRoutineButton = () => <Button>{content.start}</Button>;
