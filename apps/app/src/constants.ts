8b569b5df6b653719f3bed4854480c1ae01a582c
Steven Bister <stevebister@gmail.com>
2025-04-29 14:58:39 +0100

    add exercises route

--- apps/app/src/constants.ts
+++ apps/app/src/constants.ts
@@ -2,6 +2,7 @@ export const ROUTES = {
     ROOT: '/',
     LOGIN: '/login',
     LOGOUT: '/logout',
+    EXERCISES: '/exercises',
     WORKOUTS: '/workouts',
     ROUTINES: '/routines',
     PROFILE: '/profile',
