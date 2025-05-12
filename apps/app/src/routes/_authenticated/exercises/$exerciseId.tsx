5224884e7e8360b757e4d23a53305624a8e8a11f
Steven Bister <stevebister@gmail.com>
2025-05-12 14:43:55 +0100

    add exercise route

--- apps/app/src/routes/_authenticated/exercises/$exerciseId.tsx
+++ apps/app/src/routes/_authenticated/exercises/$exerciseId.tsx
@@ -1,9 +1,59 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from '@tanstack/react-router';
+import { useId } from 'react';
+
+import { getExercise } from '@/api/exercises';
+import { MuscleGroupList } from '@/components/muscle-group-list/muscle-group-list';
+import content from '@/content/exercises.json';
+import { getMetadata } from '@/utils/get-metadata';
 
 export const Route = createFileRoute('/_authenticated/exercises/$exerciseId')({
-  component: RouteComponent,
-})
+    loader: async ({ params }) => await getExercise(params.exerciseId),
+    component: RouteComponent,
+    head: () => ({
+        meta: getMetadata('exercises'),
+    }),
+});
 
 function RouteComponent() {
-  return <div>Hello "/_authenticated/exercises/$exerciseId"!</div>
+    const { name, howTo, primaryMuscleGroups, secondaryMuscleGroups } =
+        Route.useLoaderData();
+    const muscleGroupsId = useId();
+    const howToId = useId();
+
+    return (
+        <article>
+            <h1 className="ta-center mb-l">{name}</h1>
+
+            <section aria-labelledby={muscleGroupsId}>
+                <h2 id={muscleGroupsId} className="sr-only">
+                    {content.muscle_groups.heading}
+                </h2>
+                <dl>
+                    <MuscleGroupList
+                        muscleGroups={primaryMuscleGroups}
+                        title={content.muscle_groups.primary}
+                    />
+
+                    <MuscleGroupList
+                        muscleGroups={secondaryMuscleGroups}
+                        title={content.muscle_groups.secondary}
+                    />
+                </dl>
+            </section>
+
+            <section aria-labelledby={howToId} className="stack stack--s mt-l">
+                <h2 id={howToId} className="fw-400">
+                    {content.how_to.heading}
+                </h2>
+
+                {howTo ? (
+                    <div
+                        dangerouslySetInnerHTML={{
+                            __html: howTo,
+                        }}
+                    />
+                ) : null}
+            </section>
+        </article>
+    );
 }
---
+++ apps/app/src/tests/routes/exerciseId.test.tsx
@@ -0,0 +1,78 @@
+import { screen } from '@repo/ui/tests/utils';
+
+import { getExercise } from '@/api/exercises';
+import { ROUTES } from '@/constants';
+import content from '@/content/exercises.json';
+import { mockExercise } from '@/mocks/exercise';
+import { Route as exerciseIdRoute } from '@/routes/_authenticated/exercises/$exerciseId';
+import type { Exercise } from '@/types/api';
+
+import { mockRoute } from '../utils';
+
+vi.mock('@/api/exercises', () => ({
+    getExercise: vi.fn(),
+}));
+
+const setup = async () => {
+    vi.mocked(getExercise).mockReturnValue(
+        mockExercise as unknown as Promise<Exercise>
+    );
+
+    await mockRoute({
+        routeTree: exerciseIdRoute,
+        route: `${ROUTES.EXERCISES}/1`,
+    });
+};
+
+afterEach(() => vi.clearAllMocks());
+
+it('renders the exercise name', async () => {
+    await setup();
+
+    screen.debug();
+
+    expect(
+        screen.getByRole('heading', {
+            name: mockExercise.name,
+            level: 1,
+        })
+    ).toBeInTheDocument();
+});
+
+it('renders the target muscle groups', async () => {
+    await setup();
+
+    expect(
+        screen.getByRole('region', {
+            name: content.muscle_groups.heading,
+        })
+    ).toBeInTheDocument();
+
+    expect(screen.getByText(content.muscle_groups.primary)).toBeInTheDocument();
+    expect(
+        screen.getByText(mockExercise.primaryMuscleGroups.join(', '))
+    ).toBeInTheDocument();
+
+    expect(
+        screen.getByText(content.muscle_groups.secondary)
+    ).toBeInTheDocument();
+    expect(
+        screen.getByText(mockExercise.secondaryMuscleGroups.join(', '))
+    ).toBeInTheDocument();
+});
+
+it('renders the how to section', async () => {
+    await setup();
+
+    expect(
+        screen.getByRole('region', {
+            name: content.how_to.heading,
+        })
+    ).toBeInTheDocument();
+
+    expect(
+        screen.getByText((content) =>
+            content.includes(mockExercise.howTo!.replace(/(<([^>]+)>)/gi, ''))
+        )
+    ).toBeInTheDocument();
+});
